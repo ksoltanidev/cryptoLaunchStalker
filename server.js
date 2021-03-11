2a47e01834124d225c5176a254e30e286524674f
ksoltanidev <ksoltani.pro@gmail.com>
2021-03-11 22:30:30 +0100

    cleanup

--- server.js
+++ server.js
@@ -1,36 +1,7 @@
 var Twitter = require('twitter');
+let tweeterSecrets = require("./Secrets/tweeterSecrets");
 
-let coinbaseID = "574032254";
-
-var client = new Twitter({
-  consumer_key: 'rcegczcSvRDrEndm0qnWnCFeC',
-  consumer_secret: 'pJKtBnDEB5EIuSzLaHf4wK6iYc4Dlr2viOxVcwLKZPbSbshLfA',
-  bearer_token: 'AAAAAAAAAAAAAAAAAAAAAFaxKwEAAAAAY81kmsdntScvIw%2FQm0NOECcSzm4%3D69DnnFQX3ZVWZFH11wgp0ZWa5oKqmTgwEcywnOF3DimDIsJIie'
-});
-
-var params = {
-  q: '574032254',
-  count: 3,
-  result_type: 'recent',
-  lang: 'en'
-}
-
-/*
-//https://api.twitter.com/1.1/statuses/user_timeline.json
-client.get('search/tweets', params, function (err, data, response) {
-  if (!err) {
-    let i = 1;
-    data.statuses.forEach(tw => {
-      console.log("---------[TWEET N°" + i + "]----------");
-      console.log(tw.created_at);
-      console.log(tw.text);
-      i++;
-    })
-  } else {
-    console.log(err);
-  }
-})
-*/
+var client = new Twitter(tweeterSecrets); 
 
 let stream = client.stream('statuses/filter', { track: "javascript" })
 
