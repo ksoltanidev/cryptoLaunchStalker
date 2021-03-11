5ca6825284220a2fe6b9ca78b1ab26682d040230
ksoltanidev <ksoltani.pro@gmail.com>
2021-03-11 22:19:17 +0100

    secrets comments

--- Secrets/binanceSecrets.js
+++ Secrets/binanceSecrets.js
@@ -1,5 +1,10 @@
+/**
+ * Setup binance accounts here.
+ * owner : account owner name for logging
+ * apikey & secretKey : get from binance
+ */
 module.exports = [{
-    owner: "",
+    owner: "", 
     apikey: "",
     secretKey: ""
 }];
--- Secrets/tweeterSecrets.js
+++ Secrets/tweeterSecrets.js
@@ -1,3 +1,7 @@
+/**
+ *  tweeter account api. Used to stalk tweets.
+ */
+
 module.exports = {
     consumer_key: '',
     consumer_secret: '',
