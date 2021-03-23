b52ee9e83a9aab1732a5a8b0d0e18949ad38732f
ksoltanidev <ksoltani.pro@gmail.com>
2021-03-23 19:08:07 +0100

    tweetParse update

--- TweetParsers/isTweetCoinBaseLaunch.js
+++ TweetParsers/isTweetCoinBaseLaunch.js
@@ -1,5 +1,10 @@
 const cb = "https://t.co/sc0cuzadjl";
 
 module.exports = function isTweetCoinBaseLaunch(tweetText){
-    return (tweetText.toLowerCase().includes("launching at coinbase.com") || tweetText.toLowerCase().includes("launching at " + cb));
+    return (tweetText.toLowerCase().includes("launching")
+/*         tweetText.toLowerCase().includes("launching at coinbase.com") 
+        || tweetText.toLowerCase().includes("launching at " + cb)
+        || tweetText.toLowerCase().includes("launching on coinbase.com") 
+        || tweetText.toLowerCase().includes("launching on " + cb) */
+        );
 }
